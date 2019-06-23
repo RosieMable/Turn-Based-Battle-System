ab53a788815b5bfa95d2fca9b1a526e2047c21c5
RosieMable <sciarrillo.nella@gmail.com>
2019-06-23 15:25:52 +0100

    Git Attributes + UI + BaseClasses

---
+++ TurnedBasedCombat/Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseEnemy
+{
+
+    public string name;
+
+    public enum Type
+    {
+        FIRE,
+        GRASS,
+        WATER,
+        ELECTRIC
+    }
+
+    public enum Rarity
+    {
+        COMMON,
+        UNCOMMON,
+        RARE,
+        SUPERRARE
+    }
+
+    public Type EnemyType;
+    public Rarity EnemyRarity;
+
+    public float baseHP;
+    public float currentHP;
+
+    public float baseMP;
+    public float currentMP;
+
+    public float baseATK;
+    public float curATK;
+    public float baseDEF;
+    public float curDEF;
+}
